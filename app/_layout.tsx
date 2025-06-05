cc1460299ec6194e0f0628958916a833643cd546
waris <m.waris390@gmail.com>
2025-06-05 14:08:32 +0500

    final code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,82 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { useFonts } from "expo-font";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import Toast from "react-native-toast-message";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+const Logo = require("@/assets/icons/medicine.png");
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const [loaded, error] = useFonts({
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
   });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
+  if (!loaded && !error) {
     return null;
   }
 
+  const logoTitle = () => {
+    return (
+      <View style={styles.logo}>
+        <Image
+          source={Logo}
+          resizeMode="contain"
+          style={{
+            width: 32,
+            height: 32,
+          }}
+        />
+        {/* <AntDesign name="dingding" size={28} color="#f1fbfc" /> */}
+        <Text style={styles.logoText}>MedicAsk</Text>
+      </View>
+    );
+  };
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView
+          style={{ flex: 1, backgroundColor: "#39c3d1" }}
+          edges={["top"]}
+        >
+          <StatusBar style={"auto"} />
+          <Stack
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: "#39c3d1",
+              },
+              contentStyle: {
+                backgroundColor: "#f1fbfc",
+                paddingHorizontal: 20,
+                paddingBottom: 20,
+              },
+            }}
+          >
+            <Stack.Screen name="index" options={{ headerTitle: logoTitle }} />
+          </Stack>
+          <Toast />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </>
   );
 }
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#39c3d1",
+  },
+  logo: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  logoText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#f1fbfc",
+    fontFamily: "Poppins-Bold",
+  },
+});
